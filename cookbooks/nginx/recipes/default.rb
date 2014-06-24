16e45a2756b725caedf284218310eaf17f747b5d
Henry Turner <henry.turner@unboxedconsulting.com>
2014-06-24 15:53:44 +0100

    stop the nginx recipe from adding stuff to the database instances

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,15 +1,19 @@
-directory "/data/nginx" do
-  owner "root"
-  group "root"
-  mode 0755
-  action :create
-end
+if ['app_master', 'app', 'util', 'solo'].include?(node[:instance_role])
 
-template "/data/nginx/http-custom.conf" do 
-  owner "deploy" 
-  group "deploy" 
-  mode 0755 
-  source "http-custom.conf" 
-  backup false 
-  action :create 
+  directory "/data/nginx" do
+    owner "root"
+    group "root"
+    mode 0755
+    action :create
+  end
+
+  template "/data/nginx/http-custom.conf" do 
+    owner "deploy" 
+    group "deploy" 
+    mode 0755 
+    source "http-custom.conf" 
+    backup false 
+    action :create 
+  end
+
 end
